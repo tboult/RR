15da28872d499044dac800794b2b8ed311e0f21f
tboult <tboult@vast.uccs.edu>
2026-08-02 20:02:51 -0700

    Split into proper PWA app

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE_NAME = 'random-robin-v1';
+const ASSETS = ['./', './index.html', './manifest.json', './icon.png'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
